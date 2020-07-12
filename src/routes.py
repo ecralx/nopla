9c381ca7de4ad5b93be84710f63185f793bea9b7
Achraf Gharbi <achraf.ghb@gmail.com>
2020-07-12 20:40:51 +0200

    Intial code

---
+++ src/routes.py
@@ -0,0 +1,137 @@
+from src import app, socketio, mongo
+from flask import render_template
+from flask_socketio import join_room, emit, send
+from random import choices
+from datetime import datetime, timedelta
+from fuzzywuzzy import fuzz
+from uuid import uuid4
+
+ROOMS = {}
+DATE_FORMAT = "%Y/%m/%d %H:%M:%S"
+THRESHOLD = timedelta(seconds=60)
+HINT_THRESHOLD = timedelta(seconds=2)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+def get_random_expression():
+    return next(mongo.db.expressions.aggregate([{ "$sample": { "size": 1 } }]))
+
+def get_random_word(sentence):
+    words = sentence.split(' ')
+    words_weights = []
+    for word in words:
+        score = 0
+        if word[-1] in [',', ':', ';']:
+            score += 10
+        if word == words[-1]:
+            score += 10
+        if len(word) > 3:
+            score += 100
+        if len(word) == 1:
+            score = 0
+        words_weights.append(score)
+    return choices(words, weights=words_weights, k=1)[0]
+
+def obfuscate_in_sentence(sentence, word_to_obfuscate):
+    words = sentence.split(' ')
+    output = []
+    for word in words:
+        if word == word_to_obfuscate:
+            output += ['_' if letter not in [',', ':', ';', '-', "'", '’'] else letter for letter in word] + [' ']
+        else:
+            output += list(f'{word} ')
+    return ''.join(output[:-1])
+
+def reconstruct_sentence(sentence, answer, correct_answer):
+    output = []
+    words = sentence.split(' ')
+    for word in words:
+        if word == correct_answer:
+            output.append({"text": f'{answer} ({correct_answer}) ', "style": 'answer' })
+        else:
+            output.append({"text": f'{word} ', "style": 'normal' })
+    return output
+
+def generate_user_game_state(game_state):
+    user_game_state = game_state.copy()
+    del user_game_state['correct_answer']
+    del user_game_state['original_sentence']
+    return user_game_state
+
+@socketio.on('create')
+def on_create(data):
+    user_id = data.get('user_id')
+    if not user_id:
+        # emit("join_room", {'error': 'no user id provided', 'status':'error'})
+        user_id = str(uuid4())
+    expression = get_random_expression()
+    sentence = expression['text']
+    random_word = get_random_word(sentence)
+    user_sentence = obfuscate_in_sentence(sentence, random_word)
+    correct_answer = random_word if random_word[-1] not in [',', ':', ';'] else random_word[:-1]
+    game_state = {
+        'user_id': user_id,
+        'score': 0,
+        'answers': [],
+        'user_sentence': user_sentence,
+        'correct_answer': correct_answer,
+        'original_sentence': sentence,
+        'started_on': datetime.now().strftime(DATE_FORMAT),
+    }
+    user_game_state = generate_user_game_state(game_state)
+    room = game_state['user_id']
+    ROOMS[room] = game_state
+    join_room(room)
+    emit("join_room", {'game_state': user_game_state, 'status': 'ok'})
+
+@socketio.on('solve')
+def on_solve(data):
+    user_id = data.get('user_id')
+    answer = data.get('answer')
+    if not user_id:
+        emit("update", {'error': 'no user id provided', 'status': 'error'})
+    if not answer:
+        emit("update", {'error': 'no answer provided', 'status': 'error'})
+
+    game_state = ROOMS.get(user_id)
+    if not game_state:
+        emit("update", {'error': 'user doesnt have a game_state started', 'status': 'error'})
+    if datetime.now() - datetime.strptime(game_state['started_on'], DATE_FORMAT) > THRESHOLD:
+        emit("update", {'error': 'game finished', 'status': 'finished'})
+    
+    correct_answer = game_state['correct_answer']
+    original_sentence = game_state['original_sentence']
+    is_correct_answer = fuzz.WRatio(answer, correct_answer) > 90
+
+    game_state['answers'].append({'answer': answer, 'sentence': reconstruct_sentence(original_sentence, answer, correct_answer), 'is_correct': is_correct_answer})
+    if is_correct_answer:
+        game_state['score'] = game_state['score'] + 1
+    
+    next_expression = get_random_expression()
+    next_sentence = next_expression['text']
+    next_random_word = get_random_word(next_sentence)
+    next_user_sentence = obfuscate_in_sentence(next_sentence, next_random_word)
+    next_correct_answer = next_random_word if next_random_word[-1] not in [',', ':', ';'] else next_random_word[:-1]
+    game_state['user_sentence'] = next_user_sentence
+    game_state['correct_answer'] = next_correct_answer
+    game_state['original_sentence'] = next_sentence
+    game_state['last_time_answered'] = datetime.now().strftime(DATE_FORMAT)
+    
+    ROOMS[user_id] = game_state
+    user_game_state = generate_user_game_state(game_state)
+    emit("update", {'game_state': user_game_state, 'status': 'ok'})
+
+@socketio.on('check')
+def on_check(data):
+    user_id = data.get('user_id')
+    if not user_id:
+        emit("update", {'error': 'no user id provided', 'status': 'error'})
+    game_state = ROOMS.get(user_id)
+    if not game_state:
+        emit("update", {'error': 'user doesnt have a game_state started', 'status': 'error'})
+    if datetime.now() - datetime.strptime(game_state['started_on'], DATE_FORMAT) > THRESHOLD:
+        emit("update", {'error': 'game finished', 'status': 'finished'})
+    user_game_state = generate_user_game_state(game_state)
+    emit("update", {'game_state': user_game_state, 'status': 'ok'})
---
+++ src/static/js/main.js
@@ -0,0 +1,152 @@
+var BACKEND_URL = "http://10.31.105.9:5000"
+var socket = io.connect(BACKEND_URL);
+var gameLaunched = false;
+var gameFinished = false;
+var userId;
+var gameState = {}
+var interval;
+
+
+var FULL_DASH_ARRAY = 283;
+var TIME_LIMIT = 60;
+var WARNING_THRESHOLD = 30;
+var ALERT_THRESHOLD = 15;
+var COLOR_CODES = {
+    info: {
+        color: "green"
+    },
+    warning: {
+        color: "orange",
+        threshold: WARNING_THRESHOLD
+    },
+    alert: {
+        color: "red",
+        threshold: ALERT_THRESHOLD
+    }
+};
+var remainingPathColor = COLOR_CODES.info.color;
+document.getElementById('base-timer-path-remaining').classList.add(remainingPathColor)
+
+// verify our websocket connection is established
+socket.on('connect', function() {
+    console.log('Websocket connected!');
+});
+// message handler for the 'join_room' channel
+socket.on('join_room', function(resp) {
+    console.log('join room', resp);
+    if (resp.status == 'ok') {
+        gameLaunched = true;
+        gameState = resp.game_state
+        userId = gameState.user_id
+        document.getElementById('pt1').style.display='none';
+        document.getElementById('pt2-score').textContent=gameState.score
+        document.getElementById('pt2-time').textContent=Math.max(0, (60 - Math.floor(((new Date()) - (new Date(gameState.started_on))) / 1000))).toString()
+        document.getElementById('pt2-expression-text').textContent = gameState.user_sentence
+        document.getElementById('pt2').style.display='block';
+        document.getElementById('pt2-input').focus();
+        interval = window.setInterval(check, 1000, userId)
+    }
+});
+socket.on('update', function(resp) {
+    console.log('update', resp);
+    if (resp.status == 'ok') {
+        gameState = resp.game_state;
+        document.getElementById('pt2-score').textContent=gameState.score;
+        var timeLeft = Math.max(0, (60 - Math.floor(((new Date()) - (new Date(gameState.started_on))) / 1000)));
+        document.getElementById('pt2-time').textContent= timeLeft.toString();
+        document.getElementById('pt2-expression-text').textContent = gameState.user_sentence
+        setCircleDasharray(timeLeft);
+        setRemainingPathColor(timeLeft);
+        if (gameState.answers.length > 0) {
+            var isCorrect = gameState.answers[gameState.answers.length - 1].is_correct === true;
+            setBordersOnAnswer(isCorrect)
+        }
+    }
+    if (resp.status == 'finished') {
+        gameFinished = true;
+        window.clearInterval(interval);
+        document.getElementById('pt2').style.display='none';
+        document.getElementById('pt3').style.display='block';
+        document.getElementById('pt3-score').textContent='Tu as eu: ' + gameState.score + ' points'
+        document.getElementById('pt3-ul').innerHTML = ''
+        gameState.answers.forEach(function(answer) {
+            var isUserAnswerCorrect = answer.is_correct;
+            var node = document.createElement('li');
+            answer.sentence.forEach(function (word) {
+                var wordSpan = document.createElement('span');
+                wordSpan.textContent = word.text;
+                if (word.style == 'answer') {
+                    wordSpan.className = isUserAnswerCorrect ? 'correct' : 'incorrect';
+                }
+                node.appendChild(wordSpan);
+            });
+            document.getElementById('pt3-ul').appendChild(node)
+        })
+    }
+})
+// createGame onclick - emit a message on the 'create' channel to 
+// create a new gameState with default parameters
+function createGame() {
+    console.log('Creating gameState...');
+    socket.emit('create', {user_id: userId});
+}
+function solve() {
+    console.log('Sending an answer...');
+    var answer = document.getElementById('pt2-input').value;
+    socket.emit('solve', {user_id: userId, answer: answer});
+    document.getElementById('pt2-input').value = '';
+    document.getElementById('pt2-input').focus();
+}
+function check(id) {
+    socket.emit('check', {user_id: id});
+}
+function onKeyPress(e) {
+    if (e.keyCode == 13) {
+        solve()
+    }
+}
+function replay() {
+    window.location.reload()
+}
+function calculateTimeFraction(timeLeft) {
+    var rawTimeFraction = timeLeft / TIME_LIMIT;
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+// Update the dasharray value as time passes, starting with 283
+function setCircleDasharray(timeLeft) {
+    var circleDasharray = (calculateTimeFraction(timeLeft) * FULL_DASH_ARRAY).toFixed(0).toString() + ' 283';
+    document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", circleDasharray);
+}
+function setRemainingPathColor(timeLeft) {
+    // If the remaining time is less than or equal to 5, remove the "warning" class and apply the "alert" class.
+    if (timeLeft <= COLOR_CODES.alert.threshold) {
+        document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(COLOR_CODES.warning.color);
+        document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(COLOR_CODES.alert.color);
+
+    // If the remaining time is less than or equal to 10, remove the base color and apply the "warning" class.
+    } else if (timeLeft <= COLOR_CODES.warning.threshold) {
+        document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(COLOR_CODES.info.color);
+        document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(COLOR_CODES.warning.color);
+    }
+}
+function setBordersOnAnswer(isCorrect) {
+    if (isCorrect) {
+        document.getElementById('pt2-score-circle').classList.remove('incorrect');
+        document.getElementById('pt2-score-circle').classList.add('correct');
+        document.getElementById('pt2-input').classList.remove('incorrect');
+        document.getElementById('pt2-input').classList.add('correct');
+    } else {
+        document.getElementById('pt2-score-circle').classList.add('incorrect');
+        document.getElementById('pt2-score-circle').classList.remove('correct');
+        document.getElementById('pt2-input').classList.add('incorrect');
+        document.getElementById('pt2-input').classList.remove('correct');
+    }
+}
