de37430e02e0a967e0cfc2111eafdb37d814734e
Achraf Gharbi <achraf.ghb@gmail.com>
2020-10-07 18:48:24 +0200

    Stock rooms with redis

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,16 @@
-from dotenv import load_dotenv
+import sys
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
 import os
-#from redis import Redis
-
-
-load_dotenv()
+from redis import from_url as redis_from_url
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET')
-app.config["MONGO_URI"] = os.getenv('MONGODB_URI')
+app.config['MONGO_URI'] = os.getenv('MONGODB_URI')
+app.config['REDIS_URL'] = os.getenv('REDIS_URL')
 mongo = PyMongo(app)
-#redis = Redis()
+redis = redis_from_url(app.config['REDIS_URL'])
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 from .routes import *
